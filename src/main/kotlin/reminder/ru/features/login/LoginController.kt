24d50d5a3dc783cc5526a5350eabd1e1a7d0d61f
makzoyn <makzoyn@gmail.com>
2023-03-27 20:24:19 +0300

    -create register and login -create databases users and tokens

---
+++ src/main/kotlin/reminder/ru/features/login/LoginController.kt
@@ -0,0 +1,39 @@
+package reminder.ru.features.login
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import reminder.ru.database.tokens.TokenDTO
+import reminder.ru.database.tokens.Tokens
+import reminder.ru.database.users.Users
+import java.util.*
+
+class LoginController(private val call: ApplicationCall){
+
+    suspend fun performLogin() {
+        val receive = call.receive<LoginReceiveRemote>()
+        val userDTO= Users.fetchUser(receive.login)
+
+        if (userDTO == null){
+            call.respond(HttpStatusCode.BadRequest, "User not found")
+        }
+        else{
+            if (userDTO.password == receive.password){
+                    val token = UUID.randomUUID().toString()
+                Tokens.insert(
+                    TokenDTO(
+                        rowId = UUID.randomUUID().toString(),
+                        login = receive.login,
+                        token = token
+                    )
+                )
+                call.respond(LoginResponseRemote(token = token))
+            }
+            else
+            {
+                call.respond(HttpStatusCode.BadRequest, "Invalid password")
+            }
+        }
+    }
+}
